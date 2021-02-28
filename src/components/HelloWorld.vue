a0ea3a768f306be31f77741f6568894e69aed201
KyleBCox <kylecoxforweb@gmail.com>
2021-02-28 09:39:21 +0000

    project created

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -36,7 +36,9 @@
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 
-@Component
+@Component({
+  
+})
 export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
 }
